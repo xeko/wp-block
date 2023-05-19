f2cf422facfb43a8479251c2bb930d2559e731b5
TongHoa <hoaitvn@gmail.com>
2023-05-19 11:44:58 +0900

    Sap xep lai cau truc thu muc: update thu muc build

---
+++ build/blocks/block-one/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'd215f14dedf151005a04');
---
+++ build/blocks/block-two/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'dc80a9599b88c3279db4');
