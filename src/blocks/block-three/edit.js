7e19c2bd2a017b1db5d310948903adf8b5138ae9
TongHoa <hoaitvn@gmail.com>
2023-05-30 10:25:11 +0900

    edit...

--- src/blocks/block-three/edit.js
+++ src/blocks/block-three/edit.js
@@ -1,122 +1,42 @@
 import { __ } from '@wordpress/i18n';
-
-import { 
+// InspectorControlsはブロック選択したときのサイドバー部分
+import {
     useBlockProps,
     RichText,
     BlockControls,
-    AlignmentToolbar,
     InspectorControls,
+    AlignmentToolbar,
     PanelColorSettings,
     ContrastChecker,
+    withColors,
 } from '@wordpress/block-editor';
-
-import {
-    ToolbarGroup, 
-    ToolbarButton,
-    ToolbarDropdownMenu,
-    PanelBody,
-    TextControl,
-    TextareaControl,
-    ToggleControl,
-    ColorPalette,
-    AnglePickerControl,
-    ColorPicker,
-} from '@wordpress/components';
 
 import './editor.scss';
 
-export default function Edit({attributes, setAttributes}) {
+function Edit( props ) {
     const {
-        text, 
-        alignment, 
-        backgroundColor, 
+        attributes,
+        setAttributes,
+        backgroundColor,
         textColor,
-    } = attributes;
-
-    const onChangeText = (newText) => {
-        setAttributes({text : newText});
+        setBackgroundColor,
+        setTextColor,
+    } = props;
+    // console.log( attributes,props );
+    
+    // attributesはblock.jsonで定義したattributes
+    const { text, alignment } = attributes;
+    const onChangeText = ( newText ) => {
+        setAttributes( { text: newText } );
+    };
+    const onChangeAlignment = ( newAligment ) => {
+        setAttributes( { alignment: newAligment } );
     };
 
-    const onChangeAlignment = (newAlignment) => {
-        setAttributes({alignment: newAlignment});
-    };
-    const onBackgroundColorChange = (newBgColor) => {
-        setAttributes({backgroundColor: newBgColor});
-    };
-
-    const onTextColorChange = (newTextColor) => {
-        setAttributes({textColor: newTextColor});
-    };
-
-	return (
+    return (
         <>
-            <BlockControls group="inline">
-                <p>inline Control</p>
-            </BlockControls>
-
-            <BlockControls group="block">
-                <p>block Control</p>
-            </BlockControls>
-
-            <BlockControls
-                group="other"
-                controls = {[
-                    {
-                        title: 'ボタン１',
-                        icon: 'admin-generic',
-                        isActive: true,
-                        onClick: ()=> console.log('ボタン１ click')
-                    },
-                    {
-                        title: 'ボタン２',
-                        icon: 'admin-collapse',
-                        onClick: () => console.log("ボタン２ click")
-                    },
-                ]}
-            >
-                {text && (
-                    <ToolbarGroup>
-                        <ToolbarButton
-                            title="左揃え"
-                            icon="editor-alignleft"
-                            onClick={() => console.log("左揃え")}
-                        />
-                        <ToolbarButton
-                            title="中央揃え"
-                            icon="editor-aligncenter"
-                            onClick={() => console.log("中央揃え")}
-                        />
-                        <ToolbarButton
-                            title="右揃え"
-                            icon="editor-alignright"
-                            onClick={() => console.log("右揃え")}
-                        />
-                        <ToolbarDropdownMenu
-                            icon="arrow-down-alt2"
-                            label="他のオペション"
-                            controls={[
-                                {
-                                    title:"幅広",
-                                    icon: 'align-wide',
-                                },
-                                {
-                                    title: "全幅",
-                                    icon: 'align-full-width',
-                                }
-                            ]}
-                        />
-                    </ToolbarGroup>
-                )}
-                <ToolbarGroup>
-                    <p>Text text 1</p>
-                </ToolbarGroup>
-            </BlockControls>
-
-            <BlockControls>
-                <AlignmentToolbar/>
-            </BlockControls>            
-
             <InspectorControls>
+                { /* functions.phpで設定したeditor-color-paletteから色を取得する */ }
                 <PanelColorSettings
                     title={ __( '色の設定', 'text-box' ) }
                     icon="admin-appearance"
@@ -124,31 +44,35 @@ export default function Edit({attributes, setAttributes}) {
                     disableCustomColors={ false }
                     colorSettings={ [
                         {
-                            value: backgroundColor,
-                            onChange: onBackgroundColorChange,
+                            value: backgroundColor.color,
+                            onChange: setBackgroundColor,
                             label: __( '背景色', 'text-box' ),
                         },
                         {
-                            value: textColor,
-                            onChange: onTextColorChange,
+                            value: textColor.color,
+                            onChange: setTextColor,
                             label: __( '文字色', 'text-box' ),
                         },
                     ] }
                 >
-                {/* コントラストがうすすぎないかチェック */}
-                <ContrastChecker
-                    textColor={ textColor }
-                    backgroundColor={ backgroundColor }
-                />
+                    <ContrastChecker
+                        textColor={ textColor.color }
+                        backgroundColor={ backgroundColor.color }
+                    />
                 </PanelColorSettings>
             </InspectorControls>
-
+            <BlockControls>
+                <AlignmentToolbar
+                    onChange={ onChangeAlignment }
+                    value={ alignment }
+                />
+            </BlockControls>
             <RichText
                 { ...useBlockProps( {
                     className: `text-box-align-${ alignment }`,
                     style: {
-                        backgroundColor,
-                        color: textColor,
+                        backgroundColor: backgroundColor.color,
+                        color: textColor.color,
                     },
                 } ) }
                 onChange={ onChangeText }
@@ -157,5 +81,11 @@ export default function Edit({attributes, setAttributes}) {
                 tagName="h4"
             />
         </>
-	);
+    );
 }
+
+
+export default withColors( {
+    backgroundColor: 'backgroundColor',
+    textColor: 'color',
+} )( Edit );
--- src/blocks/block-three/save.js
+++ src/blocks/block-three/save.js
@@ -1,24 +1,50 @@
-/**
- * React hook that is used to mark the block wrapper element.
- * It provides all the necessary props like the class name.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
- */
-import { useBlockProps } from '@wordpress/block-editor';
+import {
+    useBlockProps,
+    RichText,
+    getColorClassName,
+} from '@wordpress/block-editor';
+// classNameを取得するのに必要
+// npm i classnames
+import classnames from 'classnames';
 
-/**
- * The save function defines the way in which the different attributes should
- * be combined into the final markup, which is then serialized by the block
- * editor into `post_content`.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#save
- *
- * @return {WPElement} Element to render.
- */
-export default function save() {
-	return (
-		<p { ...useBlockProps.save() }>
-			{ 'Block Three – hello from the saved content!' }
-		</p>
-	);
+export default function save( { attributes } ) {
+    // custom~はカラーパレットの自由選択で選ばれたとき
+    const {
+        text,
+        alignment,
+        backgroundColor,
+        textColor,
+        customBackgroundColor,
+        customTextColor,
+    } = attributes;
+
+    // backgroundClassは指定色ならクラスあり、自由選択で選ばれたときはclass名はundefinedになる
+    const backgroundClass = getColorClassName(
+        'background-color',
+        backgroundColor
+    );
+    const textClass = getColorClassName( 'color', textColor );
+
+    // classnamesは左辺がtrueなら、右辺のクラスを追加できる
+    // 追加したい要素がすでにclassNameが指定されている場合、第一引数に指定して、第二引数にclassnamesで判定したいものをいれる。
+    const classes = classnames( `text-box-align-${ alignment }`, {
+        [ backgroundClass ]: backgroundClass,
+        [ textClass ]: textClass,
+    } );
+    return (
+        // RichTextは編集可能なものだけどsave画面では不要なのでContentにする
+        <RichText.Content
+            { ...useBlockProps.save( {
+                className: classes,
+                style: {
+                    backgroundColor: backgroundClass
+                        ? undefined
+                        : customBackgroundColor,
+                    color: textColor ? undefined : customTextColor,
+                },
+            } ) }
+            tagName="h4"
+            value={ text }
+        />
+    );
 }
